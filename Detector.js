3a001f4e4a1f70170a98bff1de99c79cb7675f85
barry-kn <carrybarryklein49@gmail.com>
2023-03-19 15:16:32 +0300

    5th commit

--- Detector.js
+++ Detector.js
@@ -3,11 +3,14 @@
 const carSpeed = parseInt(prompt("Enter the speed of the car: "));
 //the car speed is checked is it is more than 70 is false the output is==ok
 if (carSpeed  < 70) {
-    console.log("Ok");
+    console.log("Ok")
+    
+}
     // Math.floor() function to round down to the nearest integer. 
     //This formula subtracts 70 from the speed to get the number of 
     //km/s above the speed limit, then divides that number by 5 to get the number of demerit points.
-} else {
     const demeritPoints = Math.floor((carSpeed - 70) / 5);
     console.log(`You have ${demeritPoints} demerit points.`);
+ if (demeritPoints>=12 ){
+   console.log ("license suspende")
 }
