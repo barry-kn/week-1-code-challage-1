27849e8950dac0ddf913d3a5475306f9ed23b37a
barry-kn <carrybarryklein49@gmail.com>
2023-03-20 08:12:47 +0300

    7th commit

--- tax.js
+++ tax.js
@@ -53,59 +53,98 @@ console.log(`NSSF Deduction: ${nssfAmount}`);
 console.log(`Gross salary: ${grossSalary}`);
 console.log(`Net salary: ${netSalary}`);
 */
-
- // Define constants for tax rates and other deductions
- const KRA_TAX_RATES = [[0, 12298, 0],
- [12299, 23885, 10],
- [23886, 35472, 15],
- [35473, 47059, 20],
- [47060, 58646, 25],
- [58647, Infinity, 30]];
- const NHIF_RATES = [[0, 5999, 150],
- [6000, 7999, 300],
- [8000, 11999, 400],
- [12000, 14999, 500],
- [15000, 19999, 600],
- [20000, 24999, 750],
- [25000, 29999, 850],
- [30000, 34999, 900],
- [35000, 39999, 950],
- [40000, 44999, 1000],
- [45000, 49999, 1100],
- [50000, 59999, 1200],
- [60000, 69999, 1300],
- [70000, 79999, 1400],
- [80000, 89999, 1500],
- [90000, 99999, 1600],
- [100000, 109999, 1700],
- [110000, 119999, 1800],
- [120000, 129999, 1900],
- [130000, 139999, 2000],
- [140000, 149999, 2100],
- [150000, 159999, 2200],
- [160000, 169999, 2300],
- [170000, 179999, 2400],
- [180000, 189999, 2500],
- [190000, 199999, 2600],
- [200000, Infinity, 2700]];
- const NSSF_RATE = 0.06;
  
- // Get input for basic salary and benefits
- const basicSalary = parseFloat(prompt("Enter your basic salary: "));
- const benefits = parseFloat(prompt("Enter your benefits: "));
- 
- // Calculate gross salary
- const grossSalary = basicSalary + benefits;
- 
- // Calculate PAYE tax
- let payeTax = 0;
- for (let i = 0; i < KRA_TAX_RATES.length; i++) {
- if (grossSalary > KRA_TAX_RATES[i][0] && grossSalary <= KRA_TAX_RATES[i][1]) {
- payeTax = (grossSalary - KRA_TAX_RATES[i][0]) * (KRA_TAX_RATES[i][2] / 100);
- break;
- }
- }
- 
- // Calculate NHIF deduction
- let nhifDeduction = 0
- 
+//this function takes gross salary and benefits  
+// net = grossSallary - (kra + nssf + nhif)
+function calculateTax(grossSalary, benefits){
+    
+    kra = calcKra(grossSalary);
+    nssf = calcNSSF(grossSalary);
+    nhif = calcNHIF(grossSalary);
+    netPay = grossSalary + benefits - (kra + nssf + nhif);
+    return netPay;
+}
+// getting kra
+function calcKra(grossSalary){
+    let kra_tax = 0;
+    if (grossSalary <= 24000){ //10%
+        kra_tax = (10/100) * grossSalary;
+    } else if (grossSalary >= 24001 && grossSalary <= 32333 ){//25%
+        kra_tax = (25/100) * grossSalary;
+    } else {//30%
+        kra_tax = (30/100) * grossSalary;
+    }
+    return kra_tax;
+}
+// function that calculates nssf_tax.
+function calcNSSF(grossSalary){
+    let nssf = 0;
+    if(grossSalary <= 6000){ //6%
+        nssf = (6/100) * grossSalary;
+   }
+    else if(grossSalary >= 6001 && grossSalary <= 18000){
+        nssf = (6/100) * grossSalary;
+    }
+    else{
+        nssf = (5/100) * grossSalary;
+    }
+    return nssf;
+}
+// function that calculates nhif_tax.
+function calcNHIF(grossSalary){
+    let nhif = 0;
+    if(grossSalary <= 5999){
+        nhif = 150
+    }
+    else if(grossSalary >= 6000 && grossSalary <= 7999){
+        nhif = 300;
+    }
+    else if(grossSalary >= 8000 && grossSalary <= 11999){
+        nhif = 400;
+    }
+    else if(grossSalary >= 12000 && grossSalary <= 14999){
+        nhif = 500;
+    }
+    else if(grossSalary >= 15000 && grossSalary <= 19999){
+        nhif = 600;
+    }
+    else if(grossSalary >= 20000 && grossSalary <= 24999){
+        nhif = 750;
+    }
+    else if(grossSalary >= 25000 && grossSalary <= 29999){
+        nhif = 850;
+    }
+    else if(grossSalary >= 30000 && grossSalary <= 34999){
+        nhif = 900;
+    }
+    else if(grossSalary >= 35000 && grossSalary <= 39999){
+        nhif = 950;
+    }
+    else if(grossSalary >= 40000 && grossSalary <= 44999){
+        nhif = 1000;
+    }
+    else if(grossSalary >= 45000 && grossSalary <= 49999){
+        nhif = 1100;
+    }
+    else if(grossSalary >= 50000 && grossSalary <= 59999){
+        nhif = 1200;
+    }
+    else if(grossSalary >= 60000 && grossSalary <= 69999){
+        nhif = 1300;
+    }
+    else if(grossSalary >= 70000 && grossSalary <= 79999){
+        nhif = 1400;
+    }
+    else if(grossSalary >= 80000 && grossSalary <= 89999){
+        nhif = 1500;
+    }
+    else if(grossSalary >= 90000 && grossSalary <= 99999){
+        nhif = 1600;
+    }
+    else{
+        nhif = 1700;
+    }
+    return nhif;
+}
+let final_net = calculateTax(12345554, 5375);
+console.log("your Net Pay : "+final_net); 
